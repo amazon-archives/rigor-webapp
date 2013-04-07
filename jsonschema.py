df805ccf9bd2abc050fba390aa5be397518082ee
Rabbit <rabbit@blindsight.com>
2013-04-06 18:47:29 -0700

    adding more tests for jsonschema

--- jsonschema.py
+++ jsonschema.py
@@ -39,7 +39,6 @@ def validate(schema, input, allowExtraKeys=False, allowMissingKeys=False, stack=
         if schemaKeys - inputKeys:
             raise MissingKeyException('missing key(s): %s'%str(list(schemaKeys-inputKeys)))
 
-
     for key, expectedVal in schema.items():
         if key not in input: continue
 
@@ -50,31 +49,74 @@ def validate(schema, input, allowExtraKeys=False, allowMissingKeys=False, stack=
 
         actualVal = input[key]
         actualType = type(actualVal)
-        if not actualType == expectedType:
-            raise WrongTypeException('in %s: key %s should be %s but is %s which is type %s'%('/'+'/'.join([str(s) for s in stack]),repr(key),expectedType,repr(actualVal),actualType))
+        if not isinstance(actualVal,expectedType):
+            # allow an int where we expected a float
+            if actualType == int and expectedType == float:
+                pass
+            else:
+                raise WrongTypeException('in %s: key %s should be %s but is %s which is type %s'%('/'+'/'.join([str(s) for s in stack]),repr(key),expectedType,repr(actualVal),actualType))
 
-        if actualType == expectedType == dict:
-            result = validate(expectedVal,actualVal,stack=stack+[key])
-            if not result: return result
+        if isinstance(actualVal,dict):
+            validate(expectedVal,actualVal,stack=stack+[key])
 
     return True
+
+
+#--------------------------------------------------------------------------------
 
 if __name__ == '__main__':
     testlib.begin('jsonschema')
 
-    schema = dict(a=str,b=str)
+    schemaType = dict(a=str,b=str)
+    schemaExample = dict(a='a',b='b')
 
     # perfect match
-    testlib.eq(   validate(schema, dict(a='a',b='b')), True, 'perfect match -> True'   )
+    testlib.eq(   validate(schemaType,    dict(a='a',b='b')), True, 'perfect match using types -> True'   )
+    testlib.eq(   validate(schemaExample, dict(a='a',b='b')), True, 'perfect match using example -> True'   )
+
+    # special-cased ok type mismatches
+    testlib.eq(   validate(dict(a=1.0), dict(a=1)), True, 'int is ok where float is expected'   )
 
     # missing keys
-    testlib.eq(   validate(schema, dict(a='a'), allowMissingKeys=True), True, 'missing keys allowed -> True'  )
-    testlib.expectException(    validate,
-                                [], 
-                                dict(schema=schema, input=dict(a='a'), allowMissingKeys=False),
+    testlib.eq(   validate(schemaType, dict(a='a'), allowMissingKeys=True), True, 'missing keys allowed -> True'  )
+    testlib.expectException(    validate, [],
+                                dict(schema=schemaType, input=dict(a='a'), allowMissingKeys=False),
                                 MissingKeyException,
                                 'missing keys not allowed -> MissingKeyException'  )
 
+    # extra keys
+    testlib.eq(   validate(schemaType, dict(a='a',b='b',c='c'), allowExtraKeys=True), True, 'extra keys allowed -> True'  )
+    testlib.expectException(    validate, [],
+                                dict(schema=schemaType, input=dict(a='a',b='b',c='c'), allowExtraKeys=False),
+                                ExtraKeyException,
+                                'extra keys not allowed -> ExtraKeyException'  )
+    # wrong type
+    testlib.expectException(    validate, [],
+                                dict(   schema=dict(a=str),
+                                        input=dict(a=1)
+                                ),
+                                WrongTypeException,
+                                'str is not int (type check): wrongTypeException'  )
+    testlib.expectException(    validate, [],
+                                dict(   schema=dict(a='a'),
+                                        input=dict(a=1)
+                                ),
+                                WrongTypeException,
+                                'str is not int (example check): wrongTypeException'  )
+    testlib.expectException(    validate, [],
+                                dict(   schema=dict(a=1),
+                                        input=dict(a=1.1)
+                                ),
+                                WrongTypeException,
+                                'float is not int (example check): wrongTypeException'  )
+    testlib.expectException(    validate, [],
+                                dict(   schema=dict(a=int),
+                                        input=dict(a=1.1)
+                                ),
+                                WrongTypeException,
+                                'float is not int (example check): wrongTypeException'  )
+
+    # todo: tests for nested dicts
 
     testlib.end()
 
