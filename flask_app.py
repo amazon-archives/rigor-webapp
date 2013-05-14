344707736d15e7bfcb11e35cd4a07d6d1c42ad90
Rabbit <rabbit@blindsight.com>
2013-05-14 10:43:04 -0700

    pylint fixes on flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -2,21 +2,18 @@
 
 from __future__ import division
 import os
-import random
-import time
-import json
-import pprint
 
 from flask import Flask
 from flask import render_template
-from flask import send_from_directory
 from flask import send_file
 from flask import jsonify
 from flask import request
-from flask import redirect
-from flask import url_for
-from flask import flash
 from flask import abort
+
+# from flask import flash
+# from flask import send_from_directory
+# from flask import redirect
+# from flask import url_for
 
 from utils import *
 import backend
@@ -39,10 +36,10 @@ def index():
     return render_template('browse.html')
 
 @app.route('/thumb/<locator>.<ext>',methods=['GET'])
-def getThumbFile(locator,ext):
+def getThumbFile(locator, ext):
     locator = locator.replace('-','').replace('/','').replace('..','')
     ext = ext.replace('/','').replace('..','')
-    path = '/data/rigor/thumbnails/200x200/%s/%s/%s.%s'%(
+    path = '/data/rigor/thumbnails/200x200/%s/%s/%s.%s' % (
                 locator[:2],
                 locator[2:4],
                 locator.replace('-',''),
@@ -54,10 +51,10 @@ def getThumbFile(locator,ext):
         abort(404)
 
 @app.route('/image/<locator>.<ext>',methods=['GET'])
-def getImageFile(locator,ext):
+def getImageFile(locator, ext):
     locator = locator.replace('-','').replace('/','').replace('..','')
     ext = ext.replace('/','').replace('..','')
-    path = '/data/rigor/images/%s/%s/%s.%s'%(
+    path = '/data/rigor/images/%s/%s/%s.%s' % (
                 locator[:2],
                 locator[2:4],
                 locator.replace('-',''),
@@ -75,13 +72,13 @@ def searchImages():
     queryDict = {}
     keyWhitelist = 'database_name source sensor has_tags exclude_tags max_count page'.split()
     # TODO: throw error if certain keys are missing
-    for key,val in request.values.items():
+    for key, val in request.values.items():
         if key in keyWhitelist:
             if type(val) == unicode:
                 val = val.encode('utf-8')
             queryDict[key] = val
         else:
-            debugDetail('discarding unknown key: %s = %s'%(key,val))
+            debugDetail('discarding unknown key: %s = %s' % (key, val))
     if 'has_tags' in queryDict:
         queryDict['has_tags'] = queryDict['has_tags'].split(',')
     if 'exclude_tags' in queryDict:
@@ -90,7 +87,7 @@ def searchImages():
         queryDict['max_count'] = int(queryDict['max_count'])
     if 'page' in queryDict:
         queryDict['page'] = int(queryDict['page'])
-    debugMain('searchImages: %s'%queryDict)
+    debugMain('searchImages: %s' % queryDict)
     full_count, result = backend.searchImages(queryDict)
     #print pprint.pformat(result)
     return jsonify(full_count = full_count, images=result)
@@ -104,16 +101,16 @@ def getDatabaseNames():
 
 # http://ea:5000/api/v1/db/rigor/image/23659
 @app.route('/api/v1/db/<database_name>/image/<locator>', methods=['GET'])
-def getImage(database_name,locator):
-    result = backend.getImage(database_name=database_name,locator=locator)
-    debugMain('getImage.  locator = %s'%locator)
+def getImage(database_name, locator):
+    result = backend.getImage(database_name=database_name, locator=locator)
+    debugMain('getImage.  locator = %s' % locator)
     return jsonify(result)
 
 # http://ea:5000/api/v1/db/rigor/image/afa567f9f55b4283a1ead5682637ed4e/annotation
 @app.route('/api/v1/db/<database_name>/image/<locator>/annotation', methods=['GET'])
-def getImageAnnotations(database_name,locator):
-    result = backend.getImageAnnotations(database_name=database_name,locator=locator)
-    debugMain('getImageAnnotations.  locator = %s'%locator)
+def getImageAnnotations(database_name, locator):
+    result = backend.getImageAnnotations(database_name=database_name, locator=locator)
+    debugMain('getImageAnnotations.  locator = %s' % locator)
     return jsonify(d=result)
 
 # http://ea:5000/api/v1/db/rigor/source
