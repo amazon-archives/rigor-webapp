11be1ec4767a79dd1cea2990dc6375109d8b2e5a
Rabbit <rabbit@blindsight.com>
2013-10-04 15:49:22 -0700

    Prevent browser from over-caching the cropped word image

--- crowd_app.py
+++ crowd_app.py
@@ -248,7 +248,8 @@ def getWord(annotation_id):
     simulateSlow()
     word = backend.getCrowdWord(config.CROWD_DB, annotation_id)
 
-    word['image_url'] = '/word/%s.%s' % (word['annotation_id'], word['ext'])
+    randomHash = ''.join([random.choice('abcdefghijklmnopqrstuvwxyz') for ii in range(10)])
+    word['image_url'] = '/word/%s.%s?%s' % (word['annotation_id'], word['ext'], randomHash)
     del word['image_path']
 
     return jsonify(word)
