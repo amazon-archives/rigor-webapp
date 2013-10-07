020a56420f2a2c0d79dc8ed618cc5a87e143ea4f
Rabbit <rabbit@blindsight.com>
2013-10-07 00:04:39 -0700

    Crowd: change password and page titles

--- crowd_app.py
+++ crowd_app.py
@@ -30,7 +30,7 @@ def check_auth(username, password):
     """This function is called to check if a username /
     password combination is valid.
     """
-    return username == 'rigor' and password == 'rigor!!!!!'
+    return username == 'uclabs' and password == 'uclabs!!!!!'
 
 def authenticate():
     """Sends a 401 response that enables basic auth"""
