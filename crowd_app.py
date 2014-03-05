d9633e41442595cb24c590d6eeae4a29dd6faae9
Rabbit <rabbit@blindsight.com>
2014-03-05 11:08:11 -0800

    change basic http auth passwords for web apps

--- crowd_app.py
+++ crowd_app.py
@@ -30,7 +30,7 @@ def check_auth(username, password):
     """This function is called to check if a username /
     password combination is valid.
     """
-    return username == 'uclabs' and password == 'uclabs!!!!!'
+    return username == 'blindsight' and password == 'blindsight!!!!!'
 
 def authenticate():
     """Sends a 401 response that enables basic auth"""
